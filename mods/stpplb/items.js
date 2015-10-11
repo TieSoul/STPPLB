2b62650e0ca88a325e6aeecd65bc7bbecbbfed91
azum4roll <azum4roll@users.noreply.github.com>
2015-10-11 18:45:42 +0800

    7.8/10 too many brackets

--- mods/stpplb/items.js
+++ mods/stpplb/items.js
@@ -4,17 +4,12 @@ exports.BattleItems = {
 		name: "Lunchabylls",
 		num: 444,
 		spritenum: 242,
-		fling: {
-			basePower: 10
-		},
+		fling: {basePower: 10},
 		onResidualOrder: 5,
 		onResidualSubOrder: 2,
 		onResidual: function (pokemon) {
-			if (pokemon.status) {
-				this.heal(pokemon.maxhp / 8);
-			} else {
-				this.heal(pokemon.maxhp / 16);
-			}
+			if (pokemon.status) this.heal(pokemon.maxhp / 8);
+			else this.heal(pokemon.maxhp / 16);
 		},
 		gen: 7,
 		desc: "At the end of every turn, holder restores 1/16 of its max HP. Recovers 1/8th hp if statused"
@@ -23,9 +18,7 @@ exports.BattleItems = {
 		id: 'speedshoes',
 		name: 'Speed Shoes',
 		num: 445,
-		fling: {
-			basePower: 15
-		},
+		fling: {basePower: 15},
 		desc: "Doubles speed.",
 		shortDesc: "Doubles speed.",
 		onModifySpe: function (spe, pokemon) {
